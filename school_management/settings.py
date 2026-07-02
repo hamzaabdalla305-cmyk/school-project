a396e160ca52aabb0db05a6246f3eae17ba2b273
abdalla hamza <hamzaabdalla305@gmail.com>
2026-07-02 05:58:04 +0400

    تعديل settings.py لقراءة DATABASE_URL

--- school_management/settings.py
+++ school_management/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,14 +79,10 @@ WSGI_APPLICATION = 'school_management.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'school_db',
-        'USER': 'postgres',
-        'PASSWORD': '123456',  # غيّرها لكلمة السر الخاصة بك
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:123456@localhost:5432/school_db',
+        conn_max_age=600
+    )
 }
 
 
